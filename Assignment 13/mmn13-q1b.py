e78550b15b6dba8bdd56b7c30fc69811b2fc7ac7
Beri Bahar <69582371+berrybahar@users.noreply.github.com>
2024-09-01 15:15:47 +0300

    Delete Assignment 13 directory

--- Assignment 13/mmn13-q1b.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    a_list = ["apple", "banana", "carrot", "black", "box"]
-    b_list = [word.capitalize() for word in a_list if word.startswith('b')]
-    print(b_list)
